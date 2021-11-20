637dbb86166924d028afdd087bc5bd2ddba79521
Darko Horvatovic <darkohorvatovic@gmail.com>
2021-11-20 20:15:48 +0100

    Dodao IDZaposlenog u Korisnik.java

--- src/main/java/sistem/autobuskastanica/backendklase/Korisnik.java
+++ src/main/java/sistem/autobuskastanica/backendklase/Korisnik.java
@@ -2,30 +2,36 @@ package sistem.autobuskastanica.backendklase;
 
 public class Korisnik {
 
+    private int IDZaposlenog;
     private String korisnickoIme;
     private String lozinka;
 
     public static final String imeFajla = "korisnici.txt";
 
-    public Korisnik(String korisnickoIme, String lozinka) {
+    public Korisnik(int IDZaposlenog, String korisnickoIme, String lozinka) {
+        this.IDZaposlenog = IDZaposlenog;
         this.korisnickoIme = korisnickoIme;
         this.lozinka = lozinka;
     }
 
     public static Korisnik izStringa(String string) {
 
+        int IDZaposlenog = Integer.parseInt(string.substring(string.indexOf("IDZaposlenog:") + 13, string.indexOf("\t")));
+        string = string.substring(string.indexOf("\t") + 1);
+
         String korisnickoIme = string.substring(string.indexOf("KorisnickoIme:") + 14, string.indexOf("\t"));
         string = string.substring(string.indexOf("\t") + 1);
 
         String lozinka = string.substring(string.indexOf("Lozinka:") + 8);
 
-        return new Korisnik(korisnickoIme, lozinka);
+        return new Korisnik(IDZaposlenog, korisnickoIme, lozinka);
 
     }
 
     public String uString() {
 
         StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("IDZaposlenog:").append(IDZaposlenog).append("\t");
         stringBuilder.append("KorisnickoIme:").append(korisnickoIme).append("\t");
         stringBuilder.append("Lozinka:").append(lozinka);
         return stringBuilder.toString();
